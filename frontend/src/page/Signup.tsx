be0b78b6aaf749c6c1afac781aba9e53e0064c6e
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-03-05 11:09:44 +1300

    メールアドレスによるユーザ登録機能の追加

--- frontend/src/page/Signup.tsx
+++ frontend/src/page/Signup.tsx
@@ -1,13 +1,28 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
+import { auth } from "../lib/firebase";
 
 export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault(); // ボタンが押されてもリロードしない
 
-    // Firebaseの認証をここに書く
+    try {
+      const result = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      const user = result.user;
+      const idToken = user.getIdToken();
+      console.log(`User is ${user} | ID Token is ${idToken}`);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(`Error: ${error.message}`);
+      }
+    }
   };
 
   return (
