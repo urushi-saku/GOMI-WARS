9cb6c6272deb53eff43f509967cd814e6bb7cc56
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-03-09 09:40:40 +1300

    ハンバーガーアイコンを含むナビゲーションとこれらの実装に合わせたリファクタリング

--- frontend/src/page/Home.tsx
+++ frontend/src/page/Home.tsx
@@ -1,37 +1,25 @@
-import { onAuthStateChanged, signOut } from "firebase/auth";
-import { Link } from "react-router-dom";
-import { auth } from "../lib/firebase";
-import { useEffect, useState } from "react";
-import type { User } from "firebase/auth";
+// import { onAuthStateChanged } from "firebase/auth";
+import HamburgerIcon from "../components/HamburgerIcon";
+// import { auth } from "../lib/firebase";
+// import { useEffect, useState } from "react";
+// import type { User } from "firebase/auth";
 import MapContainer from "../components/MapContainer";
 
 export default function Home() {
-  const [user, setUser] = useState<User | null>(null);
+  /* const [user, setUser] = useState<User | null>(null);
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user);
     });
     return () => unsubscribe();
   }, []);
-  const handleSignOut = async () => {
-    try {
-      await signOut(auth);
-    } catch (error) {
-      console.error("Failed to sign out:", error);
-      window.alert(
-        "ログアウトに失敗しました。時間をおいて再度お試しください。",
-      );
-    }
-  };
+  */
 
-  const logoutButton = <button onClick={handleSignOut}>ログアウト</button>;
+  // コメント部分はゴミ投稿ボタンで利用する
+
   return (
     <div>
-      <Link to="/signup">ユーザ登録画面へ</Link>
-      <br />
-      <Link to="/login">ログイン画面へ</Link>
-      <br />
-      {user ? logoutButton : null}
+      <HamburgerIcon />
       <MapContainer></MapContainer>
     </div>
   );
